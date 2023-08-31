7ff423e71fdda9f297c7bf2d8af11276d5f7213a
Randy Kofi Ansah <randyansah97@gmail.com>
2023-08-30 19:26:59 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(
+    name='main',
+    version="0.0.1",
+    description="""
+    A natural language program to:
+        1==PREDICT WHICH TOP IS DISCUSSED IN  A SENTENCE
+        2==CLASSIFY WHETHER A SENTENCE IS POSITIVE OR NEGATIVE
+        3==PREDICTING MISSING WORDS IN A SENTENCE
+        4==GENERATE TEXT
+        5==IDENTIFY ENTITIES SUCH AS ORGANIZATION,PERSONS OR LOCATIONS
+        6==SUMMARIZATION
+        7==TRANSLATION
+        8==GET VE9CTOR EXTRACTION OF A TEXT
+        9==ANSWERING QUESTIONS
+
+""",
+author="Randy Kofi Ansah",
+author_email="randyansah97@gmail.com"
+)
